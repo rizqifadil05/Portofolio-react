7e85cbe26ae6e4fc0e5906b37f245090b110eea6
rizqifadil05 <mrizqifadillah05@gmail.com>
2024-12-11 08:29:08 +0700

    porto

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -55,7 +55,7 @@ export const NavBar = () => {
                 <a href="https://www.instagram.com/rizzqe/"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+                <button className="vvd"><span>Kirim Ulasan</span></button>
               </HashLink>
             </span>
           </Navbar.Collapse>
